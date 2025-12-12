9983e1e78e5c76d013e5c0538101c26d3370c3da
Enrique Suica <enri124589@gmail.com>
2025-12-12 13:42:31 -0300

    Initialize Maven project with main class
    Add initial pom.xml for Maven configuration targeting Java 17 and create the main Concesionaria class with a basic 'Hello World!' entry point.

---
+++ src/main/java/com/edsuica/concesionaria/Concesionaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edsuica.concesionaria;
+
+/**
+ *
+ * @author Enrique
+ */
+public class Concesionaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
